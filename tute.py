bbe4221b57d40f482decc3e96a960f2b221f4869
Shbhankar <Shubhankar118@gmail.com>
2021-06-20 22:57:03 +0530

    init

---
+++ tute.py
@@ -0,0 +1,80 @@
+'''va1 = 10
+va2 = 67
+
+var3 = int(input())
+
+if var3>va2:
+    print("bigger")
+elif var3 == va2:
+    print("equal")
+else:
+    print("smaller")'''
+
+'''list1 = [1,2,3,4,5]
+print(15 not in list1)
+if 5 not in list1:
+    print("no")'''
+
+'''print("what is ur age")
+val1 = 18
+val = int(input())
+
+if val > val1:
+    print("yes u can drive")
+elif val == val1:
+    print("u have to wait")
+else:
+    print("u can not")'''
+
+'''print("Enter first number   ")
+N1 = int(input())
+
+print("Enter your operation + , - , / , * ")
+op = (input())
+
+print("Enter 2nd number   ")
+N2 = int(input())
+
+if (N1 == 45 and N2 == 3 or N1 == 3 and N2 == 45) and op == '*':
+    print(N1, op, N2, " = ", 555)
+
+elif (N1 == 56 and N2 == 4 or N1 == 4 and N2 == 56) and op == '+':
+    print(N1, op, N2, " = ", 77)
+
+elif (N1 == 56 and N2 == 6 or N1 == 6 and N2 == 5656) and op == '/':
+    print(N1, op, N2, " = ", 4)
+
+else:
+    if op == '+':
+        print(N1, op, N2, " = ", N1 + N2)
+
+    elif op == '*':
+        print(N1, op, N2, " = ", N1 * N2)
+
+    elif op == '/':
+        print(N1, op, N2, " = ", N1 / N2)
+
+    elif op == '-':
+        print(N1, op, N2, " = ", N1 - N2)
+
+    else:
+        print("over")'''
+
+
+
+'''list1=[["harry",3],["larry",2],["carry",1],["marry",32]]
+
+dict1=dict(list1)
+for item,lollypop in dict1.items():
+    print(item,lollypop)'''
+
+#items =[int,float, "harry" ,7,9,88,56,34,65,11]
+
+#for item in items:
+ #   if str(item).isnumeric() and item >= 10:
+  #      print(item)
+
+i=100
+while (i>20):
+    print(i)
+    i = i-1
---
+++ veronika.py
@@ -0,0 +1,72 @@
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import datetime
+import webbrowser
+import os
+
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("goodmorning")
+    elif hour>12 and hour<18:
+        speak("good afternoon")
+    else:
+        speak("good evening")
+    speak("Hello Boss I am Veronica and tell me How may i assist you")
+
+def takeCommand():
+    '''It take microphone input from user'''
+
+    r = sr.Recognizer()
+    with sr.Microphone() as sourse:
+        print("listening.....")
+        r.pause_threshold = 1#seconds of non speaking audio before we consider the speaking audio a phrase
+        audio = r.listen(sourse)
+
+    try:
+        print("recognizing....")
+        query =r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        #print(e)
+        print("say that again.....")
+        return "None"
+    return query
+
+if __name__ == '__main__':
+    wishMe()
+    while True:
+        query = takeCommand().lower()  #converted to lower string
+
+                                 #logic executing tasks based on query
+
+        if 'wikipedia' in query:
+          speak('searching wikipedia...')
+          query = query.replace("wikipedia", "")
+          results = wikipedia.summary(query, sentences=2)
+          speak("According to wikipedia ")
+          print(results)
+          speak(results)
+
+        elif 'open youtube' in query:
+              webbrowser.open("youtube.com")
+        elif 'open google' in query:
+              webbrowser.open("google.com")
+        elif 'open stack over flow' in query:
+              webbrowser.open("stackoverflow.com")
+
