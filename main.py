8a2341bc59ca3be737ba9d9595bbbbab7cfd3ab7
Shbhankar <Shubhankar118@gmail.com>
2021-06-23 15:22:52 +0530

    updated

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-with open("abc.txt") as f:
-    a=f.read(4)
-    print("Shub")
+print(voice)
--- veronika.py
+++ veronika.py
@@ -4,7 +4,7 @@ import wikipedia
 import datetime
 import webbrowser
 import os
-
+import smtplib
 
 
 
@@ -19,6 +19,7 @@ def speak(audio):
     engine.runAndWait()
 
 def wishMe():
+    
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
         speak("goodmorning")
@@ -26,12 +27,12 @@ def wishMe():
         speak("good afternoon")
     else:
         speak("good evening")
-    speak("Hello Boss I am Veronica and tell me How may i assist you")
+    speak("Hello Sir I am Veronica and tell me How may i assist you")
 
 def takeCommand():
-    '''It take microphone input from user'''
+    #It take microphone input from user and return string outputs
 
-    r = sr.Recognizer()
+    r = sr.Recognizer() #recognizer helps to recognize the voice 
     with sr.Microphone() as sourse:
         print("listening.....")
         r.pause_threshold = 1#seconds of non speaking audio before we consider the speaking audio a phrase
@@ -48,17 +49,27 @@ def takeCommand():
         return "None"
     return query
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('zayns470@hmail.com','your-password')
+    server.sendmail('zayns470@gmail.com', to, content)
+    server.close() 
+
+
+
 if __name__ == '__main__':
-    wishMe()
-    while True:
+     wishMe()
+while True:
         query = takeCommand().lower()  #converted to lower string
-
-                                 #logic executing tasks based on query
-
+        
+        
+        #logic executing tasks based on query
         if 'wikipedia' in query:
           speak('searching wikipedia...')
           query = query.replace("wikipedia", "")
-          results = wikipedia.summary(query, sentences=4)
+          results = wikipedia.summary(query, sentences=2)
           speak("According to wikipedia ")
           print(results)
           speak(results)
@@ -67,6 +78,27 @@ if __name__ == '__main__':
               webbrowser.open("youtube.com")
         elif 'open google' in query:
               webbrowser.open("google.com")
-        elif 'open stack over flow' in query:
+        elif 'open stack overflow' in query:
               webbrowser.open("stackoverflow.com")
 
+        elif 'play music' in query:
+            music_dir =  'D:\\NARUTO\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'the time ' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"sir, the time is {strTime}")
+        elif 'open code ' in query:
+            codePath = "C:\\Users\\Shubhankar\\AppData\\Local\\Program\Microsoft VS Code"
+            os.startfile(codePath)
+        elif 'send email' in query:
+            try: 
+                speak("what shound i say")
+                content =takeCommand()
+                to = "abhayr625@gmail.com"
+                sendEmail(to, content)
+                speak("email has been sent!")
+            except Exception as e:
+                #print(e)
+                speak("sir right now you did not gave me permission to send emils")
